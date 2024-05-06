fc6bd2c604f570ef59d217dc6c78ba5b035b0948
yuzhangkai <53356318+yuzhangkaii@users.noreply.github.com>
2024-05-06 13:27:08 +0800

    Update tshark.sh

--- peizhi/tshark.sh
+++ peizhi/tshark.sh
@@ -6,17 +6,12 @@ DURATION=$1
 
 
 
-capture_command="tshark -n -t ad -Y 'http.host == hostip' -T fields -e frame.time -e ip.src -e http.host -e http.request.uri -l  > log.txt"
 
-
-
-    eval "$capture_command" &
-    pid=$!
-
+tshark_pid=$(nohup tshark -n -t ad -Y 'http.host == hostip' -T fields -e frame.time -e ip.src -e http.host -e http.request.uri -l  > log.txt& echo $!)
     # 等待指定的抓包时长后，结束tshark进程
     sleep $DURATION
     echo "Stopping current tshark capture."
-    kill $pid
+    kill $tshark_pid
 
     # 确保tshark进程已终止
     wait $pid 2>/dev/null
